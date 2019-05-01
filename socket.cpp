ab117bc12c10f1c8f23fe6826175ef244d223bea
Koki Imai <gonchi282@gmail.com>
2019-05-01 18:12:19 +0900

    20190501

--- socket.cpp
+++ socket.cpp
@@ -1,26 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <iostream>
-#include <sstream>
+#include "socket.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//#include <unistd.h>
+//#include <netdb.h>
+//#include <openssl/ssl.h>
+//#include <openssl/err.h>
+//#include <iostream>
+//#include <sstream>
+//
+//#define BUFSIZE 32
+//#define IPSIZE 16
+//#define OK 0
+//#define NG -1
+//
+//#define DNT_WRITE 0
+//#define SEND_ERR -1
 
-#define BUFSIZE 32
-#define IPSIZE 16
-#define OK 0
-#define NG -1
+int main(int argc, char **argv)
+{
+    int send_size;                  // 書き込みサイズ
+    int recv_size;                  // 受信サイズ
+    int sock;                       // ソケットディスクリプタ
+    int portNo = 443;                // ポート番号 
+    std::string hostname;           // ホストネーム
+    std::string ipaddr_str;         // IPアドレス
+    std::string recvBuffer;         // 受信データ
+    std::string path;               // リクエストするpath
+    std::stringstream ss;           // stringstream
+    struct sockaddr_in client_addr; // ソケットアドレス構造体
+    in_addr_t addr;            // 第一引数がアドレスか判定用
+    SSL *ssl;                       // SSL構造体
+    SSL_CTX *ctx;
+    
 
-#define DNT_WRITE 0
-#define SEND_ERR -1
+    // コマンド引数検証
+    if(argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <hostname> <request path>\n", argv[0]);
+        return OK;
+    }
+    hostname = argv[1];     // コマンドライン第一引数をホストネーム
+    path = argv[2];         // コマンドライン第二引数をリクエストパスに
 
-#define ABC
+    addr = inet_addr(hostname.c_str());
+    if(addr == INADDR_NONE)
+    {
+        // hostnameがホストネームの時
+        ipaddr_str = resolve_hostname(hostname);
+    }
+    else
+    {
+        // hostnameがIPアドレスの時
+        ipaddr_str = hostname;
+    }
+
+    // ソケット生成
+    if((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket() failed");
+        exit(NG);
+    }
+
+    // sockaddr_in構造体に接続するアドレス・ポート番号を設定
+    memset(&client_addr, 0, sizeof(struct sockaddr_in));            // 0クリアする
+    client_addr.sin_family = AF_INET;                               // IPv4
+    client_addr.sin_addr.s_addr = inet_addr(ipaddr_str.c_str());    
+    client_addr.sin_port = htons(portNo);
+
+    // ソケットをサーバーに接続
+    if(connect(sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) != 0)
+    {
+        perror("connect() failed");
+        exit(NG);
+    }
+    std::cout << "connect to " << ipaddr_str << std::endl;
+
+    // SSLセットアップ
+    SSL_load_error_strings();
+    SSL_library_init();
+
+    ctx = SSL_CTX_new(SSLv23_client_method());
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+    SSL_connect(ssl);
+
+    ss << "GET /" << path << " HTTP/1.1\r\n";
+    std::cout << ss.str();
+    send_size = send_request_by_ssl(ssl, ss.str());
+    ss.str("");
+    ss << "HOST: " << hostname << "\r\n";
+    std::cout << ss.str();
+    send_size = send_request_by_ssl(ssl,ss.str());
+    send_request_by_ssl(ssl, "Connection: Close\r\n\r\n");
+
+    // レスポンスを受信
+    do
+    {
+        recvBuffer = recv_response_by_ssl(ssl, BUFSIZE);
+        recvBuffer[recvBuffer.length()] = '\0';
+        printf("%s", recvBuffer.c_str());
+    }while(recvBuffer.length() > 0);
+
+
+
+    // SSL終了処理
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    ERR_free_strings();
+
+    // ソケットを閉じる
+    close(sock);
+
+    return OK;
+}
 
 // リクエスト送信
 int send_request(int sock, std::string sendMsg)
@@ -113,83 +211,3 @@ std::string resolve_hostname(std::string hostname)
     return ipaddr;
 }
 
-int main(int argc, char **argv)
-{
-    int send_size;                  // 書き込みサイズ
-    int recv_size;                  // 受信サイズ
-    int sock;                       // ソケットディスクリプタ
-    int portNo = 443;                // ポート番号 
-    std::string hostname;           // ホストネーム
-    std::string ipaddr_str;         // IPアドレス
-    std::string recvBuffer;         // 受信データ
-    struct sockaddr_in client_addr; // ソケットアドレス構造体
-    SSL *ssl;                       // SSL構造体
-    SSL_CTX *ctx;
-    
-
-    // コマンド引数検証
-    if(argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
-        return OK;
-    }
-    hostname = argv[1];
-    ipaddr_str = resolve_hostname(hostname);
-
-#ifdef ABC
-    // ソケット生成
-    if((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("socket() failed");
-        exit(NG);
-    }
-
-    // sockaddr_in構造体に接続するアドレス・ポート番号を設定
-    memset(&client_addr, 0, sizeof(struct sockaddr_in));
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = inet_addr(ipaddr_str.c_str());
-    client_addr.sin_port = htons(portNo);
-
-    // ソケットをサーバーに接続
-    if(connect(sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) != 0)
-    {
-        perror("connect() failed");
-        exit(NG);
-    }
-    std::cout << "connect to " << ipaddr_str << std::endl;
-
-    // SSLセットアップ
-    SSL_load_error_strings();
-    SSL_library_init();
-
-    ctx = SSL_CTX_new(SSLv23_client_method());
-    ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, sock);
-    SSL_connect(ssl);
-
-    send_size = send_request_by_ssl(ssl, "GET / HTTP/1.1\r\n");
-    send_size = send_request_by_ssl(ssl, "HOST: sunmoon-kanazawa.jp\r\n");
-    send_request_by_ssl(ssl, "Connection: Close\r\n\r\n");
-
-    // レスポンスを受信
-    do
-    {
-        recvBuffer = recv_response_by_ssl(ssl, BUFSIZE);
-        recvBuffer[recvBuffer.length()] = '\0';
-        printf("%s", recvBuffer.c_str());
-    }while(recvBuffer != "");
-
-
-
-    // SSL終了処理
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    SSL_CTX_free(ctx);
-    ERR_free_strings();
-
-    // ソケットをと居る
-    close(sock);
-#endif
-
-    return OK;
-}
